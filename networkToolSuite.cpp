06b261b4ef0269469a8fdb79d5260eb0cea108f5
107nicho <nbauder107@gmail.com>
2024-04-22 01:16:38 -0500

    v0.2
    - Added IP version detection function
    - Added IP address formatting
    - Made naming convention more consistent
    - Minor fixes & optimizations

--- networkToolSuite.cpp
+++ networkToolSuite.cpp
@@ -9,42 +9,59 @@
 #include <sstream>
 #include <math.h>
 
-void v4(); // Subnet process for an ipv4 address
-void v6();
+enum class ipVersion {
+    IPv4,
+    IPv6,
+    Invalid
+};
 
-int formatIP();         // Creates and stores IP address
+ipVersion getIPv(const std::string&);    // Detect IP version
+void v4();      // Subnet IPv4 address
+void v6();      // IPv6
+int subnetClassA();     // Subnets a class A network
+int subnetClassB();     // Subnets a class B network
+int subnetClassC();     // Subnets a class C network
 bool decToBin(int);     // Converts dotted decimal into binary boolean string (base 10 to base 2)
 int binToDec(bool[]);   // Converts a binary boolean string to  dotted decimal (base 2 to base 10)
 
-int subnetClassA(); // Subnets a class A network
-int subnetClassB(); // Subnets a class B network
-int subnetClassC(); // Subnets a class C network
-
 const int BITS = 8; // Global constant bits per octet (8)
-int main()
-{
-    bool valid = false;
-    std::string version;
-    std::cout << "Enter the IP version your address uses (IPv4 or IPv6): ";
-// IPv4 uses base10 to base2 and IPv6 uses hexadecimal
-    while (!valid) { // Input validation loop
-        std::getline(std::cin, version);
-        if (version == "IPv4" || version == "ipv4" || version == "v4") { // IPv4 function call
-            v4();
-            valid = true;
-        }
-        else if (version == "IPv6" || version == "ipv6" || version == "v6") { // IPv6 function fall
-            v6();
-            valid = true;
-        }
-        else { // Input validation
-            std::cout << "Invalid Internet Protocol version entered, please enter either v4 or v6: ";
+int main() {
+    std::string ipAddress;
+    ipVersion version = ipVersion::Invalid;
+
+    while (version == ipVersion::Invalid) {
+        std::cout << "Enter an IP address: ";
+        std:: cin >> ipAddress;
+
+        ipVersion version = getIPv(ipAddress);
+
+        switch (version) {
+            case ipVersion::IPv4:
+                std::cout << "The entered IP address is IPv4";
+                v4();
+                break;
+            case ipVersion::IPv6:
+                std::cout << "The entered IP address is IPv6";
+                v6();
+                break;
+            case ipVersion::Invalid:
+                std::cout << "Invalid IP version";
+                break;
+
         }
     }
 }
 
-void v4()
-{
+ipVersion getIPv(const std::string& ipAddress) {
+    if (ipAddress.find('.') != std::string::npos) {
+        return ipVersion::IPv4;
+    }
+    else if (ipAddress.find(':') != std::string::npos) {
+        return ipVersion::IPv6;
+    }
+}
+
+void v4() {
     int hosts;
     bool valid = false;
     while (!valid) // Input validation loop
@@ -59,29 +76,39 @@ void v4()
         }
     }
 
-    int IP, octet1;
+    std::string ipAddress;
+    int octets[4];
     valid = false;
     while (!valid) // Input validation loop
     {
         std::cout << "Enter the network IPv4 address in dotted decimal notation: ";
-        std::cin >> IP;
-        if (octet1 >= 0 && octet1 <= 127) {
+        std::cin >> ipAddress;
+
+        std::stringstream ipStream(ipAddress);
+        std::string octet;
+
+        for (int i = 0; i < 4; i++) {
+            std::getline(ipStream, octet, '.'); // Parses through the string and separates each octet as a string
+            octets[i] = std::stoi(octet); // Converts string octet to int
+        }
+        
+        if (octets[0] >= 0 && octets[0] <= 127) {
             subnetClassA();
             valid = true;
         }
-        else if (octet1 >= 128 && octet1 <= 191) {
+        else if (octets[0] >= 128 && octets[0] <= 191) {
             subnetClassB();
             valid = true;
         }
-        else if (octet1 >= 192 && octet1 <= 223) {
+        else if (octets[0] >= 192 && octets[0] <= 223) {
             subnetClassC();
             valid = true;
         }
-        else if (octet1 >= 224 && octet1 <= 239) {
+        else if (octets[0] >= 224 && octets[0] <= 239) {  // Multicast
             std::cout << "This is a Class D network, it has no subnet mask.";
             return;
         }
-        else if (octet1 >= 240 && octet1 <= 255) {
+        else if (octet[0] >= 240 && octets[0] <= 255) {  // Experimental
             std::cout << "This is a Class E network, it has no subnet mask.";
             return;
         }
@@ -91,14 +118,12 @@ void v4()
     }
 }
 
-void v6()
-{
+void v6() {
     std::cout << "WORK IN PROGRESS.";
     return;
 }
 
-bool decToBin(int base10) // Converts dotted decimal into binary boolean string (base 10 to base 2)
-{
+bool decToBin(int base10) { // Converts dotted decimal into binary boolean string
     bool base2[BITS] = {false};
     int pow2[BITS] = {128, 64, 32, 16, 8, 4, 2, 1};
     for (int i = 0; i < BITS; i++) {
@@ -114,21 +139,12 @@ bool decToBin(int base10) // Converts dotted decimal into binary boolean string 
 }
 
 
-int binToDec(bool base2[8]) //converts a binary boolean string to  dotted decimal (base 2 to base 10)
-{
+int binToDec(bool base2[8]) { // Converts a binary boolean string to dotted decimal notation
 // Reverse engineer decToBin function once completed
     return 0;
 }
 
-int formatIP() //formats IP address into 4 octets seperated by periods
-{
-// Use a string and parse through the string, if code runs 
-// Into a period start new number in an array of ints for each octet
-    return 0;
-}
-
-int subnetClassA() {
-// Implement subnetting for Class A network
+int subnetClassA() { // Implement subnetting for Class A network
     return 0;
 }
 
@@ -141,12 +157,3 @@ int subnetClassC() {
 // Implement subnetting for Class C network
     return 0;
 }
-
-/*  
-IDEA
-Use multidimensional array where each octet is in its own page and each bit is in its own column
-int octets[4][1][8];
-    const int OCTET_SIZE = 8;
-    for (int i = 0; i <= OCTET_SIZE; i++) {
-    }
-*/
